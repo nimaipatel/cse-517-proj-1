f68e2ed62a9bb39ab805244e01de292b565bee2e
nimai <nimai.m.patel@gmail.com>
2024-09-09 12:50:01 -0400

    make proc from oo

--- queues.py
+++ queues.py
@@ -3,6 +3,7 @@
 
 from __future__ import annotations
 from collections import defaultdict
+from dataclasses import dataclass
 import math
 import random
 from enum import Enum
@@ -11,7 +12,21 @@ from typing import List, NewType, Optional
 
 random.seed(10)
 
-job_id_t = NewType('job_id_t', int)
+event_log_file_handle = None
+
+
+def log_event(msg: str):
+    global event_log_file_handle
+
+    if event_log_file_handle == None:
+        event_log_file_handle = open("event.log", "w")
+        event_log_file_handle.truncate(0)
+
+    event_log_file_handle.write(msg)
+    event_log_file_handle.write("\n")
+
+
+job_id_t = NewType("job_id_t", int)
 
 
 class EventType(Enum):
@@ -28,7 +43,7 @@ SIM_TIME = 10000
 q1: List[job_id_t] = []
 q2: List[job_id_t] = []
 
-curr_time = 0
+curr_time: float = 0
 
 server_1_busy = False
 server_2_busy = False
@@ -39,84 +54,96 @@ total_sojourn_time: float = 0
 arrival_times: dict[job_id_t, float] = {}
 
 
-def exponential_random(mean: float):
+def exponential_random(mean: float) -> float:
     U = random.random()
     return -math.log(1 - U) / mean
 
 
+@dataclass
 class Event:
-    def __init__(self, time: float, type: EventType, job_id: job_id_t):
-        self.time = time
-        self.type = type
-        self.job_id = job_id
+    time: float
+    type: EventType
+    job_id: job_id_t
 
 
+@dataclass
 class EventStackNode:
+    event: Event
+    next: Optional[EventStackNode]
+    prev: Optional[EventStackNode]
+
     def __init__(self, event: Event):
         self.event = event
-        self.next: Optional[EventStackNode] = None
-        self.prev: Optional[EventStackNode] = None
+        self.next = None
+        self.prev = None
 
 
+def ES_Is_Empty(es: EventStack):
+    return es.head == None
+
+
+def ES_Insert(es: EventStack, event: Event):
+    node = EventStackNode(event)
+
+    if ES_Is_Empty(es):
+        es.head = node
+        es.tail = node
+        return
+
+    assert (
+        es.head != None
+    ), "head cannot be None, we checked that the queue is not empty"
+    if node.event.time <= es.head.event.time:
+        node.next = es.head
+        es.head.prev = node
+        es.head = node
+        return
+
+    assert (
+        es.tail != None
+    ), "tail cannot be None, we checked that the queue is not empty"
+    if node.event.time >= es.tail.event.time:
+        node.prev = es.tail
+        es.tail.next = node
+        es.tail = node
+        return
+
+    curr = es.head
+    while curr and curr.event.time < node.event.time:
+        curr = curr.next
+
+    assert (
+        curr != None
+    ), "curr can't be None, insertion at end of the stack is handled in O(1) already"
+    node.next = curr
+    node.prev = curr.prev
+
+    if curr.prev:
+        curr.prev.next = node
+    curr.prev = node
+
+
+def ES_Pop(es: EventStack):
+    assert es.head != None
+    event = es.head.event
+    es.head = es.head.next
+
+    if es.head:
+        es.head.prev = None
+    else:
+        es.tail = None
+
+    return event
+
+
+@dataclass
 class EventStack:
+    head: Optional[EventStackNode]
+    tail: Optional[EventStackNode]
+
     def __init__(self):
         self.head = None
         self.tail = None
-
-    def is_empty(self):
-        return self.head == None
-
-    def insert(self, event: Event):
-        node = EventStackNode(event)
-
-        if self.is_empty():
-            self.head = node
-            self.tail = node
-            return
-
-        assert (
-            self.head != None
-        ), "head cannot be None, we checked that the queue is not empty"
-        if node.event.time <= self.head.event.time:
-            node.next = self.head
-            self.head.prev = node
-            self.head = node
-            return
-
-        assert (
-            self.tail != None
-        ), "tail cannot be None, we checked that the queue is not empty"
-        if node.event.time >= self.tail.event.time:
-            node.prev = self.tail
-            self.tail.next = node
-            self.tail = node
-            return
-
-        curr = self.head
-        while curr and curr.event.time < node.event.time:
-            curr = curr.next
-
-        assert (
-            curr != None
-        ), "curr can't be None, insertion at end of the stack is handled in O(1) already"
-        node.next = curr
-        node.prev = curr.prev
-
-        if curr.prev:
-            curr.prev.next = node
-        curr.prev = node
-
-    def pop(self):
-        assert self.head != None
-        event = self.head.event
-        self.head = self.head.next
-
-        if self.head:
-            self.head.prev = None
-        else:
-            self.tail = None
-
-        return event
 
 
 es = EventStack()
@@ -128,13 +155,13 @@ def arrival():
 
     total_jobs += 1
     job_id = job_id_t(total_jobs)
-    print(f"Job {job_id} arrives at time {curr_time:.2f}")
+    log_event(f"Job {job_id} arrives at time {curr_time:.2f}")
     arrival_times[job_id] = curr_time
 
     # Schedule the next arrival (Poisson process)
     inter_arrival_time = exponential_random(ARRIVAL_RATE)
     event = Event(curr_time + inter_arrival_time, EventType.ARRIVAL, job_id)
-    es.insert(event)
+    ES_Insert(es, event)
 
     # If server for stage 1 is free, start processing the job
     if server_1_busy:
@@ -148,19 +175,19 @@ def process_stage_1(job_id: job_id_t):
     global server_1_busy
     server_1_busy = True
 
-    print(f"Job {job_id} starts service at stage 1 at {curr_time:.2f}")
+    log_event(f"Job {job_id} starts service at stage 1 at {curr_time:.2f}")
 
     # Schedule the completion of stage 1
-    service_time_1 = exponential_random(SERVICE_TIME_STAGE_1)
-    event = Event(curr_time + service_time_1, EventType.COMPLETE_STAGE_1, job_id)
-    es.insert(event)
+    serv_time = exponential_random(SERVICE_TIME_STAGE_1)
+    event = Event(curr_time + serv_time, EventType.COMPLETE_STAGE_1, job_id)
+    ES_Insert(es, event)
 
 
 def complete_stage_1(job_id: job_id_t):
     """Handles the completion of stage 1 for a job."""
     global server_1_busy, server_2_busy
 
-    print(f"Job {job_id} completes stage 1 at {curr_time:.2f}")
+    log_event(f"Job {job_id} completes stage 1 at {curr_time:.2f}")
     server_1_busy = False
 
     # If server for stage 2 is free, move the job to stage 2
@@ -180,19 +207,19 @@ def process_stage_2(job_id: job_id_t):
     global server_2_busy
     server_2_busy = True
 
-    print(f"Job {job_id} starts service at stage 2 at {curr_time:.2f}")
+    log_event(f"Job {job_id} starts service at stage 2 at {curr_time:.2f}")
 
     # Schedule the completion of stage 2
-    service_time_2 = exponential_random(SERVICE_TIME_STAGE_2)
-    event = Event(curr_time + service_time_2, EventType.COMPLETE_STAGE_2, job_id)
-    es.insert(event)
+    serv_time = exponential_random(SERVICE_TIME_STAGE_2)
+    event = Event(curr_time + serv_time, EventType.COMPLETE_STAGE_2, job_id)
+    ES_Insert(es, event)
 
 
 def complete_stage_2(job_id: job_id_t):
     """Handles the completion of stage 2 for a job."""
     global server_2_busy, total_sojourn_time, comp_jobs
 
-    print(f"Job {job_id} completes stage 2 at {curr_time:.2f}")
+    log_event(f"Job {job_id} completes stage 2 at {curr_time:.2f}")
     server_2_busy = False
 
     # Track total time in the system for the job
@@ -205,12 +232,17 @@ def complete_stage_2(job_id: job_id_t):
         process_stage_2(next_job_id)
 
 
+def free_resources():
+    if event_log_file_handle != None:
+        event_log_file_handle.close()
+
+
 def main():
     global curr_time
 
     # This is not an actual event
     event = Event(0, EventType.ARRIVAL, job_id_t(-1))
-    es.insert(event)
+    ES_Insert(es, event)
 
     start_time = 0
     queue_1_len = 0
@@ -219,9 +251,10 @@ def main():
     q1_freq: defaultdict[int, float] = defaultdict(lambda: 0)
     q2_freq: defaultdict[int, float] = defaultdict(lambda: 0)
     overall_freq: defaultdict[int, float] = defaultdict(lambda: 0)
-    while not es.is_empty() and curr_time < SIM_TIME:
-        event = es.pop()
-        curr_time, event_type, job_id = event.time, event.type, event.job_id
+    while not ES_Is_Empty(es) and curr_time < SIM_TIME:
+        event = ES_Pop(es)
+        curr_time = event.time
+
         elapsed = curr_time - start_time
         q1_freq[queue_1_len] += elapsed
         q2_freq[queue_2_len] += elapsed
@@ -231,40 +264,45 @@ def main():
         queue_2_len = len(q2)
         start_time = curr_time
 
-        if event_type == EventType.ARRIVAL:
+        if event.type == EventType.ARRIVAL:
             arrival()
-        elif event_type == EventType.COMPLETE_STAGE_1:
-            complete_stage_1(job_id)
-        elif event_type == EventType.COMPLETE_STAGE_2:
-            complete_stage_2(job_id)
+        elif event.type == EventType.COMPLETE_STAGE_1:
+            complete_stage_1(event.job_id)
+        elif event.type == EventType.COMPLETE_STAGE_2:
+            complete_stage_2(event.job_id)
         else:
             assert False, "UNREACHABLE"
 
     q1_probs = freq_to_prob(q1_freq)
-    for length, freq in q1_freq.items():
+    for length, freq in q1_probs.items():
         print(length, freq)
 
     print()
 
     q2_probs = freq_to_prob(q2_freq)
-    for length, freq in q2_freq.items():
+    for length, freq in q2_probs.items():
         print(length, freq)
 
     print()
 
     overall_probs = freq_to_prob(overall_freq)
-    for length, freq in overall_freq.items():
+    for length, freq in overall_probs.items():
         print(length, freq)
 
     prove_johnsons_theorem(q1_probs, q2_probs, overall_probs)
 
-    average_time_in_system = total_sojourn_time / comp_jobs
+    avg_sojourn_time = total_sojourn_time / comp_jobs
     print(f"Total jobs inbound: {total_jobs}")
     print(f"Total jobs completed: {comp_jobs}")
-    print(f"Average time in system: {average_time_in_system} units of time")
+    print(f"Average time in system: {avg_sojourn_time} units of time")
+
+    free_resources()
 
 
 def freq_to_prob(freqs: dict[int, float]) -> dict[int, float]:
+    """Takes dictionary that maps queue length to frequency of that queue lenght
+    and returns dictionary that maps queue length to probablity of that queue
+    lenght"""
     result: dict[int, float] = {}
     total_freq = 0
     for _, freq in freqs.items():
